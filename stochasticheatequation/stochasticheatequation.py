23537e2e97a4778886ef82abad2e8cccbcf346b7
cyberguli <guglielmo.padula@aruba.it>
2024-02-02 21:21:30 +0100

    add channel

--- stochasticheatequation/stochasticheatequation.py
+++ stochasticheatequation/stochasticheatequation.py
@@ -12,7 +12,7 @@ class StochasticHeatEquation():
         self.data_directory = os.path.join(os.path.dirname(__file__), 'data')
         self.t=torch.tensor(np.load(os.path.join(self.data_directory, 't.npy')),dtype=torch.float32)
         self.x=torch.tensor(np.load(os.path.join(self.data_directory, 'x.npy')),dtype=torch.float32)
-        self.u=torch.tensor(np.load(os.path.join(self.data_directory, 'ut.npy')),dtype=torch.float32)
+        self.u=torch.tensor(np.load(os.path.join(self.data_directory, 'ut.npy')),dtype=torch.float32).unsqueeze(-1)
         self.train_dataset=TensorDataset(self.u)
         self.train_loader=torch.utils.data.DataLoader(self.train_dataset,batch_size=batch_size,shuffle=False)
 
