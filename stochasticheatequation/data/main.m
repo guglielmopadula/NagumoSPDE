d61b0ed3d51cf6d4f35cf46785b8bd8a6cc656bb
cyberguli <guglielmo.padula@aruba.it>
2024-02-02 21:12:30 +0100

    reduced data size and squared

--- stochasticheatequation/data/main.m
+++ stochasticheatequation/data/main.m
@@ -1,7 +1,7 @@
-a=20; J=1024; x=(0:a/J:a)'; u0=1./(1+exp(-(2-x)/sqrt(2)));
-ell=1; N=1e3; T=1; epsilon=1; sigma=1;
+a=20; J=1023; x=(0:a/J:a)'; u0=1./(1+exp(-(2-x)/sqrt(2)));
+ell=1; N=1023; T=1; epsilon=1; sigma=1;
 
-tot=zeros(1025,1001,600);
+tot=zeros(1024,1024,600);
 
 for i=1:600
     disp(i)
--- stochasticheatequation/data/preprocess.py
+++ stochasticheatequation/data/preprocess.py
@@ -1,10 +1,14 @@
 import mat73
 import numpy as np
+import skimage.measure
 variables=mat73.loadmat('variables.mat')
 ut=variables['tot'].astype(np.float32)
+ut=np.transpose(ut)
+ut=skimage.measure.block_reduce(ut, (1, 16,16), np.mean)
 x=variables['x'].astype(np.float32)
 t=variables['t'].astype(np.float32)
-ut=np.transpose(ut)
+x=skimage.measure.block_reduce(x, (16), np.mean)
+t=skimage.measure.block_reduce(t, (16), np.mean)
 np.save('ut.npy',ut)
 np.save('x.npy',x)
 np.save('t.npy',t)
